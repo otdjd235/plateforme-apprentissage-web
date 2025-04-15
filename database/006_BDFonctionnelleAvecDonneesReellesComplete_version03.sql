6cf7fe6ef37e1403fed1ff032b2f57648592128b
Miguel Dumetz <migueldumetz@gmail.com>
2025-04-15 18:06:42 -0400

    cleanup

--- database/006_BDFonctionnelleAvecDonneesReellesComplete_version03.sql
+++ database/006_BDFonctionnelleAvecDonneesReellesComplete_version03.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `project_database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `project_database`;
 -- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: project_database
